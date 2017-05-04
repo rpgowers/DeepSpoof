b97cabd724fe846fade8b23968da6ff94bc5d73c
glspisso <glspisso@gmail.com>
2017-05-04 16:53:02 +0100

    added explanations

--- spoof_main.py
+++ spoof_main.py
@@ -54,10 +54,16 @@ def game_step(players,history,N,P):
 # i,l = #nr of coins 
 
 # Current frequency
-# x_i^j = e^(b*Q_i^j)/Z 
+# x_(i,v)^j = e^(Beta*Q_i^j)/Z 
 # y_i^j = OBSERVED frequency (i.e. history)
 # Update weights for choice
-# Q_i^j (t+1) = (1-alpha)Q_i^j + sum_l P(i,l)  y^k
+# Alpha = memory, 0 is full memory and equal weights to past obs.
+#								 >0 more recent more weight
+#								  0 no memory
+# Beta = 	intensity, 0 is uniformly at random pick
+#										 >0 small historical advantage increases frequency
+# Q_i^j (t+1) = (1-Alpha)Q_i^j + sum_l P(i,l)  y^k
+
 def full_game(players,n,N,realisations):
     space = (N*n+1)*(n+1)
     A = np.zeros([space,space])
